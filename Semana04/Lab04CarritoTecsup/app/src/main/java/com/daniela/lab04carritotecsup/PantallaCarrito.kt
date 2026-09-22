c630db4c0556dfc61661678e18f49b8d416035ba
danielavila-stack <daniela.vila@tecsup.edu.pe>
2026-09-21 22:24:50 -0500

    Proyecto inicial: Agrega Lab04CarritoTecsup

---
+++ Semana04/Lab04CarritoTecsup/app/src/main/java/com/daniela/lab04carritotecsup/PantallaCarrito.kt
@@ -0,0 +1,39 @@
+package com.daniela.lab04carritotecsup
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PantallaCarrito() {
+    var nombre by remember { mutableStateOf("") }
+    var precio by remember { mutableStateOf("") }
+    var cantidad by remember { mutableStateOf("") }
+
+
+    val productos = remember { mutableStateListOf<Producto>() }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Mi Carrito TECSUP") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+
+        }
+    }
+}
